67f9bb8007dfad16a871f3da947be8ef2314e37f
nidhi.shah <nidhi.shah@qmetry.com>
2020-05-26 17:12:04 +0530

    Updating Plugin

--- src/test/java/com/qmetry/qaf/example/steps/StepsLibrary.java
+++ src/test/java/com/qmetry/qaf/example/steps/StepsLibrary.java
@@ -3,6 +3,8 @@ package com.qmetry.qaf.example.steps;
 import static com.qmetry.qaf.automation.step.CommonStep.click;
 import static com.qmetry.qaf.automation.step.CommonStep.sendKeys;
 
+import org.testng.Assert;
+
 import com.qmetry.qaf.automation.step.QAFTestStep;
 
 public class StepsLibrary {
@@ -10,9 +12,24 @@ public class StepsLibrary {
 	 * @param searchTerm
 	 *            : search term to be searched
 	 */
-	@QAFTestStep(description = "search for {0}")
-	public static void searchFor(String searchTerm) {
-		sendKeys(searchTerm, "input.search");
-		click("button.search");
+	static String currentBalance = "";
+	static String amountTobeCredited = "";
+	
+	@QAFTestStep(description = "Current balance {0}")
+	public static void currentBalance(String balance) {
+		currentBalance = balance;
+		System.out.println("Current balance is " + balance);
+	}
+	
+	@QAFTestStep(description = "Credit amount {0}")
+	public static void creditAmount(String amount) {
+		System.out.println("Amount to be credited is " + amount);
+		amountTobeCredited = amount;
+		currentBalance = String.valueOf(Integer.parseInt(currentBalance) + Integer.parseInt(amountTobeCredited));
+	}
+	
+	@QAFTestStep(description = "Verify balance is {0}")
+	public static void verifyBalance(String updatedAmount) {
+		Assert.assertEquals(currentBalance, updatedAmount);
 	}
 }
--- src/test/java/com/qmetry/qaf/example/test/SampleTest.java
+++ src/test/java/com/qmetry/qaf/example/test/SampleTest.java
@@ -2,7 +2,7 @@ package com.qmetry.qaf.example.test;
 
 import static com.qmetry.qaf.automation.step.CommonStep.get;
 import static com.qmetry.qaf.automation.step.CommonStep.verifyLinkWithPartialTextPresent;
-import static com.qmetry.qaf.example.steps.StepsLibrary.searchFor;
+import static com.qmetry.qaf.example.steps.StepsLibrary.*;
 import org.testng.annotations.Test;
 
 import com.qmetry.qaf.automation.ui.WebDriverTestCase;
@@ -10,9 +10,9 @@ import com.qmetry.qaf.automation.ui.WebDriverTestCase;
 public class SampleTest extends WebDriverTestCase {
 
 	@Test
-	public void testGoogleSearch() {
-		get("/");
-		searchFor("qaf github infostretch");
-		verifyLinkWithPartialTextPresent("qaf");
+	public void verifyCreditOperation() {
+		currentBalance("2500");
+		creditAmount("500");
+		verifyBalance("3000");
 	}
 }
